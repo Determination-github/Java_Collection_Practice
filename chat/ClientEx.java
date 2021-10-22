0dabffa1c33dfb93dd85f93de0d5bf90c148ad28
Determination-github <ungseopkim@naver.com>
2021-10-22 15:00:24 +0900

    소켓 프로그래밍 간단 연습

---
+++ chat/ClientEx.java
@@ -0,0 +1,59 @@
+package chat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientEx {
+
+	public static void main(String[] args) {
+		Socket socket = null;
+		BufferedReader in = null;
+		BufferedWriter out = null;
+		Scanner sc = new Scanner(System.in);
+
+		try {
+			socket = new Socket("localhost", 2000);
+
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+			while (true) {
+				System.out.println("보내기 >>");
+				String outMsg = sc.nextLine();
+
+				if (outMsg.equalsIgnoreCase("bye")) {
+					out.write(outMsg + "\n");
+					out.flush();
+					break;
+				}
+
+				out.write(outMsg + "\n");
+				out.flush();
+
+				String inMsg = in.readLine();
+				System.out.println("서버 : " + inMsg);
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				sc.close();
+				out.close();
+				in.close();
+				socket.close();
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
